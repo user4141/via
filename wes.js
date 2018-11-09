8638bba78855e43fa5329a357f9174ff651e810d
user4141 <44729954+user4141@users.noreply.github.com>
2018-11-09 07:24:05 +0200

    Add files via upload

---
+++ wes.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+console.log(`Logged in as ${client.user.tag}!`);
+});
+
+
+client.on('ready', async() => {
+var server = "510103671313268772"; // ايدي السررفر
+var channel = "510128835895885834";//ايدي الروم
+    setInterval(()=>{
+    client.guilds.get(server).channels.get(channel).send('يا خرا شنو هذا يا خرا شنو هذا يا خرا شنو هذا يا خرا شنو هذا يا خرا شنو هذا يا خرا شنو هذا يا خرا شنو هذا ')
+	},305);
+})
+
+
+client.login("");
